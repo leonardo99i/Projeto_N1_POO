893e6d44464cfff0857ab482da83ba668adcc1cb
lauraolimo <laurinha.oliveira.99@hotmail.com>
2021-05-03 15:48:29 -0300

    Criando classe Golpes.java
    Criando a classe Golpes :construction:

--- Projeto_N1/src/projeto/Golpes.java
+++ Projeto_N1/src/projeto/Golpes.java
@@ -1,5 +1,18 @@
 package projeto;
 
 public class Golpes {
-    
+    private String nomeGolpe;
+    private int poder;
+    private String elemento;
+
+    public Golpes(String nomeGolpe, int poder, String elemento){
+        this.nomeGolpe = nomeGolpe;
+        this.poder = poder;
+        this.elemento = elemento;
+    }
+
+    @Override
+    public String toString(){
+        return "Golpes [elemento=" + elemento + ", nomeGolpe=" + nomeGolpe + ", poder=" + poder + "]";
+    }
 }
